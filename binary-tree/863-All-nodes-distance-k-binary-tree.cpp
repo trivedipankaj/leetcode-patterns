586e9b28fefd8fb7822c7fbf9fb1e3c5cf9526db
Pankaj Trivedi <ppt@Pankajs-MacBook-Pro.local>
2021-11-18 13:10:37 +0530

    binary-tree/

---
+++ binary-tree/863-All-nodes-distance-k-binary-tree.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, vector<int>> G;
+    unordered_map<int,bool> visited;
+    queue<int> q;
+    
+    vector<int> res;
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+       buildGraph(root, NULL);
+        
+       q.push(target->val);
+       visited[target->val] = true;
+       
+        int level =0;
+        while(!q.empty()){
+            int n  = q.size();
+           
+            while(n--){
+               int curr = q.front(); q.pop(); 
+               if(level == k){
+                   res.push_back(curr);
+               }
+                for(int i=0; i<G[curr].size(); i++){
+                    if(!visited[G[curr][i]]){
+                        q.push(G[curr][i]);
+                        visited[G[curr][i]] = true;
+                    }
+                }
+              
+            }
+           level++;
+        }
+         return res;  
+    }
+    
+    void buildGraph(TreeNode* root, TreeNode* parent){
+        if(!root) return;
+         
+        if(parent){
+            G[root->val].push_back(parent->val);
+            G[parent->val].push_back(root->val);
+        }
+        buildGraph(root->left, root);
+        buildGraph(root->right, root);
+        
+    }
+};
